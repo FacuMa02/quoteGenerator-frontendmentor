5477865565676d8f46cf59d76aad535d4b5b8b69
FacuMa02 <facumaidana2001@gmail.com>
2024-01-04 12:56:47 -0300

    fixed position of button and number of quote

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function generate() {
   url = "https://api.adviceslip.com/advice";
   fetch(url)
     .then((response) => response.json())
-    .then((data) => changeQuote(data.slip.advice))
+    .then((data) => changeQuote(data.slip.advice, data.slip.id))
     .catch((error) => console.log(error));
 }
 
 //shows the advice in the screen
-function changeQuote(advice) {
+function changeQuote(advice, number) {
+  let note = document.querySelector("#label-advice");
   let quote = document.querySelector("#quote");
+  note.innerHTML = `ADVICE #${number}`;
   quote.innerHTML = `"${advice}"`;
 }
